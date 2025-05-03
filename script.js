e2464d81ecd4ba81f80e7fe9030aeac26ebe644e
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2025-05-03 14:55:21 -0500

    Made more readable

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const checkButton = document.getElementById("check-button");
-const wordOneInput = document.getElementById("word-1");
-const wordTwoInput = document.getElementById("word-2");
-const resultDisplay = document.getElementById("result");
-const displayWordOne = document.getElementById("display-word-1");
-const displayWordTwo = document.getElementById("display-word-2");
+const checkButton = document.getElementById("check-button")
+const wordOneInput = document.getElementById("word-1")
+const wordTwoInput = document.getElementById("word-2")
+const resultDisplay = document.getElementById("result")
+const displayWordOne = document.getElementById("display-word-1")
+const displayWordTwo = document.getElementById("display-word-2")
+
 
 checkButton.addEventListener("click", function () {
-    const wordOne = wordOneInput.value.trim();
-    const wordTwo = wordTwoInput.value.trim();
+    const wordOne = wordOneInput.value.trim()
+    const wordTwo = wordTwoInput.value.trim()
 
-    displayWordOne.textContent = wordOne;
-    displayWordTwo.textContent = wordTwo;
+    displayWordOne.textContent = wordOne
+    displayWordTwo.textContent = wordTwo
 
-    const isAnagram = wordOne.split('').sort().join('') === wordTwo.split('').sort().join('');
-    resultDisplay.textContent = isAnagram ? "Yes, they're anagrams." : "Nope, not anagrams.";
-});
+    let wordOneConfirmation = wordOne.split('').sort().join('')
+    let wordTwoConfirmation = wordTwo.split('').sort().join('')
+
+    const isAnagram = wordOneConfirmation === wordTwoConfirmation
+    resultDisplay.textContent = isAnagram ? "Yes, they're anagrams." : "Nope, not anagrams."
+})
